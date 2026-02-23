5650acbe7fa463da982552d5d8a1bcb4313174e9
MariaJoby <mariajobypunnassery@gmail.comm>
2026-02-23 11:17:34 +0530

    backend init done

---
+++ Backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// ===== Middleware =====
+app.use(cors());
+app.use(express.json());
+
+// ===== Test Route =====
+app.get("/", (req, res) => {
+  res.send("EduFlow Backend is Running 🚀");
+});
+
+// ===== Import Routes (Others will add here later) =====
+// Example:
+// const questionRoutes = require("./routes/questionRoutes");
+// app.use("/api/question", questionRoutes);
+
+// ===== MongoDB Connection =====
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch((err) => console.log("DB Error:", err));
+
+// ===== Start Server =====
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
